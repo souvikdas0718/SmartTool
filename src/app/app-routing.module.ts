80fbfdd224b1d6d0dfb566a655ec4fd5105cfc20
Ian Page <Ian.Page@dal.ca>
2020-11-18 22:57:18 -0400

    added login to routes and made index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Components import
+import { LoginComponent }   from './login/login.component';
 
-const routes: Routes = [];
+// URL path definitions
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },  // Index page
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
