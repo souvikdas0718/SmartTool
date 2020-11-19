1e73d4f13d010fb725f0ae3978b7fd2844eee401
Ian Page <Ian.Page@dal.ca>
2020-11-19 12:34:18 -0400

    defined user object

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,33 @@
 export class User {
+    uid: number;
+    user_name: string;
+    avg_wage: number;
+    num_employees: number;
+
+    constructor() {
+        this.uid = -1;
+        this.user_name = "";
+        this.avg_wage = 0;
+        this.num_employees = 0;
+    }
+
+    // Adds data to empty User object
+    setData(uid: number, user_name: string, avg_wage: number, num_employees: number) {
+        this.uid = uid;
+        this.user_name = user_name;
+        this.avg_wage = avg_wage;
+        this.num_employees = num_employees;
+    }
+
+    setUID(uid: number){ this.uid = uid; }
+    getUID(): number { return this.uid; }
+
+    setUsername(user_name: string) { this.user_name = user_name; }
+    getUsername(): string { return this.user_name; }
+
+    setAvgWage(avg_wage: number) { this.avg_wage = avg_wage; }
+    getAvgWage(): number { return this.avg_wage; }
+
+    setNumEmployees(num_employees: number) { this.num_employees = num_employees; }
+    getNumEmployees(): number { return this.num_employees; }
 }
