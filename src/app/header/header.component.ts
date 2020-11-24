d5471ec87c717fad4a8e7b93d93b6d3450a8e016
Ian Page <Ian.Page@dal.ca>
2020-11-24 13:33:40 -0400

    added logout functionality to nav-bar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private cookieService: CookieService) { }
 
   ngOnInit(): void {
   }
 
+  logout() {
+    this.cookieService.set('current_user', "");
+    this.router.navigate(['/login']);
+  }
 }
