4336b62c875433e49f5cfd118562685291b46036
Ian Page <Ian.Page@dal.ca>
2020-11-24 14:20:05 -0400

    removed client form from routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ import { MainComponent } from './main/main.component'
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { GraphAnalyticsComponent } from './graph-analytics/graph-analytics.component';
-import { ClientFormComponent } from './client-form/client-form.component';
 import { ClientDisplayComponent } from './client-display/client-display.component';
 
 // URL path definitions
@@ -16,7 +15,6 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   {path:'smartanalysis',component: GraphAnalyticsComponent},
-  {path: 'clientform', component: ClientFormComponent},
   {path: 'clients', component: ClientDisplayComponent}
 ];
 
