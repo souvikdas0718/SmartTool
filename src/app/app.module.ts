f072c82cb565fa880096f2d688ee2fe380356373
Anudish <an455254@dal.ca>
2020-11-24 11:25:02 -0400

    Components inter-connected and header setup for application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AlertModalComponent } from './alert-modal/alert-modal.component';
 import { MainComponent } from './main/main.component';
 import { CookieService } from 'ngx-cookie-service';
 import { GraphAnalyticsComponent } from './graph-analytics/graph-analytics.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { GraphAnalyticsComponent } from './graph-analytics/graph-analytics.compo
     SignupComponent,
     AlertModalComponent,
     MainComponent,
-    GraphAnalyticsComponent
+    GraphAnalyticsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
