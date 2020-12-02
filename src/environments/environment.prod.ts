e03dbe371f31f239427fb526df35bf201f10716c
Anudish <an455254@dal.ca>
2020-12-02 00:45:44 -0400

    Modified environment.prod.ts file to resolve production firebase connection conflicts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: 'http://localhost:3000',
+  firebaseConfig: {
+    apiKey: "AIzaSyAFUHYMn-RrXVn2HT_uObUZYJwpVb_ZoIA",
+    authDomain: "team8-smarttool.firebaseapp.com",
+    databaseURL: "https://team8-smarttool.firebaseio.com",
+    projectId: "team8-smarttool",
+    storageBucket: "team8-smarttool.appspot.com",
+    messagingSenderId: "155532596940",
+    appId: "1:155532596940:web:f5e62f6e2b61c95061d469",
+    measurementId: "G-MZECGEPP9H"
+  }
 };
