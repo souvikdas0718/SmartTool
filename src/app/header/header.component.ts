7c35f9281368ec90e6710211a19e83c58af30ae1
Ian Page <Ian.Page@dal.ca>
2020-12-02 18:15:23 -0400

    fixed security bug

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 import {Router} from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 
@@ -7,13 +7,26 @@ import { CookieService } from 'ngx-cookie-service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, DoCheck {
+
+  loggedIn: boolean;
 
   constructor(
     private router: Router,
     private cookieService: CookieService) { }
 
+  // Hiding access to full nav-bar if logged out
+  ngDoCheck(): void {
+    var current_uid = this.cookieService.get('current_user');
+    if(current_uid == "") {
+      this.loggedIn = false;
+    } else {
+      this.loggedIn = true;
+    }
+  }
+
   ngOnInit(): void {
+    this.loggedIn = false;
   }
 
   logout() {
