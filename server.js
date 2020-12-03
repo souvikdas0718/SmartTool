b5debfd1a63eea28ab9cb194e9285b6f6841c49b
Anudish <an455254@dal.ca>
2020-12-02 21:48:59 -0400

    Update in package.json

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/team8-smart-tool'));
+app.use(express.static(__dirname + '/team8_technology_innovation'));
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/team8-smart-tool/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/team8_technology_innovation/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
