faa779abcaa07154d6f135c16a66201868982f0c
Anudish <an455254@dal.ca>
2020-12-03 18:57:09 -0400

    Update in server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/team8_technology_innovation'));
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/team8_technology_innovation/index.html'));
+    res.sendFile(path.join(__dirname + '/team8_technology_innovation/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
